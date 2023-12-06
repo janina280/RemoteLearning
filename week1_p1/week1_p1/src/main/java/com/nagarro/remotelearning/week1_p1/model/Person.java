b9a1b5792a3fef04a2ba5441b6d74d333656a79e
Janina <coceijanina@yahoo.com>
2023-12-06 10:54:56 +0200

    Update problem

---
+++ week1_p1/week1_p1/src/main/java/com/nagarro/remotelearning/week1_p1/model/Person.java
@@ -0,0 +1,42 @@
+package com.nagarro.remotelearning.week1_p1.model;
+
+public class Person {
+    private final String firstName;
+    private final String lastName;
+    private final String dob;
+    private  String dod = null;
+
+
+    public Person(String string) {
+        // Splitting the string based on a comma
+        String[] textInput = string.split(",");
+
+        // Assigning the first field to firstName
+        firstName = textInput[0];
+
+        // Assigning the second field to lastName
+        lastName = textInput[1];
+
+        // Assigning the third field to dob (date of birth)
+        dob = textInput[2];
+
+        // Checking if there is a fourth field (date of death)
+        if (textInput.length < 4) {
+
+            // If not, set "Still alive" as the value for dod (date of death)
+            dod = "Still alive";
+        } else {
+
+            // If there is, assign the value of the fourth field to dod (date of death)
+            dod = textInput[3];
+        }
+
+    }
+
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + "(" + dob + " " + dod+" )";
+
+    }
+}
---
+++ week1_p1/week1_p1/src/main/java/com/nagarro/remotelearning/week1_p1/tools/ReadFromFile.java
@@ -0,0 +1,25 @@
+package com.nagarro.remotelearning.week1_p1.tools;
+
+import java.io.*;
+
+public class ReadFromFile {
+
+
+    public InputStream getFileFromResourceAsStream(String fileName) {
+
+        //creating 'classLoader' object
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        //input stream for the specified file
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
+
+    }
+
+}
