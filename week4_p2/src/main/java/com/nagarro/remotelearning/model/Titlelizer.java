0c01895ebf3b1d6efb5f212d8596963e050fc9ae
Janina <coceijanina@yahoo.com>
2024-02-27 12:43:59 +0200

    Code refactoring

--- week4_p2/src/main/java/com/nagarro/remotelearning/model/Titlelizer.java
+++ week4_p2/src/main/java/com/nagarro/remotelearning/model/Titlelizer.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Titlelizer {
-    private static final int FIRST_LETTER_INDEX = 0;
     private static final int START_INDEX = 0;
     private static final String BLANK_SPACE = " ";
     private static final List<String> IGNORED_WORDS = new ArrayList<>(Arrays.asList("the", "a", "to", "in", "of"));
@@ -13,7 +12,8 @@ public class Titlelizer {
     public String titlelize(String toTitlelize) {
         if (toTitlelize == null) {
             throw new IllegalArgumentException();
-        } else if (toTitlelize.equals("")) {
+        }
+        if ("".equals(toTitlelize)){
             return "";
         }
         String[] splittedString = toTitlelize.split(BLANK_SPACE);
@@ -24,10 +24,11 @@ public class Titlelizer {
     private String capitalizeFirstLetterOfEachWord(String[] splittedString) {
         StringBuilder newString = new StringBuilder();
         for (int i = START_INDEX; i < splittedString.length; i++) {
-            if (isNotIgnoredWord(splittedString[i])) {
-                newString.append(Character.toUpperCase(splittedString[i].charAt(FIRST_LETTER_INDEX))).append(splittedString[i].substring(1)).append(BLANK_SPACE);
+            String word = splittedString[i];
+            if (isNotIgnoredWord(word)) {
+                newString.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1)).append(BLANK_SPACE);
             } else {
-                newString.append(splittedString[i]).append(BLANK_SPACE);
+                newString.append(word).append(BLANK_SPACE);
             }
         }
         newString.deleteCharAt(newString.length() - 1);
